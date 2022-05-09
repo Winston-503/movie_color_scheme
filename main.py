d0cfed64f0c75acd914fc8daf004486ab9e8caa0
Winston-90 <nikolaevdmitryprog@gmail.com>
2022-05-09 18:09:56 -0400

    Update code files: change numpy saving to pickle and keep adding basic functionality

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from video_color_scheme import VideoColorScheme
 
+# TODO: parse arguments
+
 if __name__ == '__main__':
     video_path = 'videos/short.mp4'  # path to the video to process
     step = 1.  # get video frame each 'step' seconds
     number_of_colors = 5  # number of colors to extract from each image
+    height, weight = 100, 100  # size of the result image
 
     v = VideoColorScheme(video_path=video_path, result_path='.', temp_folder='tmp', delete_after=False)
 
-    v.video_to_images(step=step)  # convert initial video to array of images
-    v.images_to_colors(number_of_colors=number_of_colors)  # extract the most popular image colors
-    v.compose_result_image(mode='popular')  # compose a result image from an array of colors
+    images_path = v.video_to_images(step)  # convert initial video to array of images
+    colors_path = v.images_to_colors(images_path, number_of_colors)  # extract the most popular image colors
+    result_colors = v.choose_colors(colors_path, mode='popular')  # choose the colors for the result image
+    v.compose_result_image(result_colors, height, weight)  # compose a result image from an array of colors
--- video_color_scheme.py
+++ video_color_scheme.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 from collections import Counter
 
 import cv2
@@ -6,6 +7,8 @@ import moviepy.editor as mp
 import numpy as np
 from sklearn.cluster import KMeans
 from tqdm import tqdm
+
+
 # from matplotlib import pyplot as plt
 
 
@@ -70,21 +73,22 @@ class VideoColorScheme:
             self.images.append(image)  # add it into list
 
         # save images array to disk
-        self.images = np.array(self.images)
-        np.save(self.images_path, self.images)
+        save(self.images, self.images_path)
 
         print(f"Video file {self.video_path} was successfully converted to an image array")
         print(f"Images saved into {self.images_path} file\n")
 
         return self.images_path
 
-    def images_to_colors(self, images_path=None, number_of_colors=5):
+    def images_to_colors(self, images_path=None, number_of_colors=5, compress_to=300):
         """
         Read a list of images from 'images_path' and convert them into the most popular colors.
 
         :param images_path: (str) path to saved numpy array of images produced by 'video_to_images()' function.
             If None, use the value stored in the current object.
         :param number_of_colors: (int)
+        :param compress_to: (int) number of pixels to resize the image during preprocessing, usually 300-500.
+            Large values slow down the analysis, small values skew its results
         :return: (str) path to saved array of colors
         """
 
@@ -92,36 +96,27 @@ class VideoColorScheme:
             images_path = self.images_path
 
         print("Start extracting the most popular color from images")
-        print(f"Images: {images_path}, number of colors: {number_of_colors}")
+        print(f"Images: {images_path}, number of colors: {number_of_colors}, compress to: {compress_to}")
 
         # read images file
-        self.images = np.load(images_path)
+        self.images = load(images_path)
         print(f"Images from {images_path} were successfully read")
 
         self.colors = []
         for image in tqdm(self.images):
-            image_colors = self.image_to_colors(image, number_of_colors)
+            image_colors = self.image_to_colors(image, number_of_colors, compress_to)
             self.colors.append(image_colors)
 
         # save colors array to disk
-        self.colors = np.array(self.colors)
-        np.save(self.colors_path, self.colors)
+        save(self.colors, self.colors_path)
 
         print(f"Images {images_path} were successfully converted to a colors array")
         print(f"Colors saved into {self.colors_path} file")
 
         return self.colors_path
 
-    def image_to_colors(self, image, number_of_colors=5, compress_to=300):
-        """
-        Extract the most popular colors from an image
-
-
-        :param image:
-        :param number_of_colors: (int) number of colors to extract
-        :param compress_to: (int) number of pixels to resize the image during preprocessing, usually 300-500.
-            Large values slow down the analysis, small values skew its results
-        """
+    def image_to_colors(self, image, number_of_colors, compress_to):
+        """ Extract the most popular colors from an image """
 
         modified_image = self.__preprocess(image, compress_to)
         return self.__analyze_colors(modified_image, number_of_colors)
@@ -140,6 +135,7 @@ class VideoColorScheme:
         Return the list of colors for the image.
         List contains of tuples (color, ratio) and contains 'number_of_colors' elements
         """
+
         def rgb_to_hex(rgb_color):
             """ Helper function to convert RGB color to HEX """
             hex_color = "#"
@@ -157,7 +153,7 @@ class VideoColorScheme:
         hex_colors = [(rgb_to_hex(ordered_colors[idx]), value / total) for idx, value in color_counts.items()]
         return hex_colors
 
-    def compose_result_image(self, colors_path=None, mode=''):
+    def choose_colors(self, colors_path=None, mode='sample'):
         """
 
         :param colors_path: (str) path to saved numpy array of colors produced by 'images_to_colors()' function.
@@ -174,7 +170,7 @@ class VideoColorScheme:
         print(f"Colors: {colors_path}, mode: {mode}")
 
         # read colors file
-        self.colors = np.load(colors_path)
+        self.colors = load(colors_path)
         print(f"Colors from {colors_path} were successfully read")
 
         print("Choosing colors for the result image...")
@@ -184,16 +180,7 @@ class VideoColorScheme:
             result_colors.append(result_color)
         print("Colors were successfully chosen")
 
-        print("Composing the result image...")
-        for result_color in tqdm(result_colors):
-            pass
-
-        # save the image
-        print("The result images was successfully composed")
-
-        if self.delete_after:
-            import shutil
-            shutil.rmtree(self.temp_folder)
+        return result_colors
 
     @staticmethod
     def __choose_color(color_tuple, mode):
@@ -211,3 +198,35 @@ class VideoColorScheme:
         else:
             colors, prob = zip(*color_tuple)
             return np.random.choice(colors, p=prob)
+
+    def compose_result_image(self, result_colors, height, weight):
+        """
+        Create the result image from an array of colors
+
+        :param result_colors: (list of str) list of the HEX colors to create an image
+        :param height: (int) height of the result image, ?
+        :param weight: (int) weight of the result image, ?
+        """
+
+        print("\nStart composing the result image...")
+        for result_color in tqdm(result_colors):
+            pass
+
+        # save the image
+        print("The result images was successfully composed")
+
+        if self.delete_after:
+            import shutil
+            shutil.rmtree(self.temp_folder)
+
+
+def save(obj, filename):
+    """ Save a Python object using pickle """
+    with open(filename, 'wb') as f:
+        pickle.dump(obj, f)
+
+
+def load(filename):
+    """ Load a Python object using pickle """
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
