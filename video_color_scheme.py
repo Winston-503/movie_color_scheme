246c80824de4ad3e45a9999b1b3ec7ba0ccaeace
Winston-90 <nikolaevdmitryprog@gmail.com>
2022-05-10 20:05:43 -0400

    Fix a bug with uni-color images and reformatting

--- video_color_scheme.py
+++ video_color_scheme.py
@@ -12,16 +12,14 @@ from tqdm import tqdm
 
 
 class VideoColorScheme:
-    def __init__(self, video_path, result_path, temp_folder, delete_after=True):
+    def __init__(self, video_path, temp_folder='tmp', delete_after=True):
         """
         :param video_path: (str) path to the video to process
-        :param result_path: (str) path to save the result image
         :param temp_folder: (str) path to store temporary files
         :param delete_after: (bool) if true, delete temporary files after processing
         """
 
         self.video_path = video_path
-        self.result_path = result_path
         self.temp_folder = temp_folder
         self.delete_after = delete_after
         self.colors = None
@@ -59,7 +57,7 @@ class VideoColorScheme:
         Convert video to the list of images. Get video frame each 'step' seconds.
         Save the list of images into 'self.images_path' file.
 
-        :param start: (float) time to start in seconds
+        :param start: (float) time to start video processing (seconds)
         :param step: (float) step in seconds
         :return (str) path to saved array of images
         """
@@ -83,10 +81,12 @@ class VideoColorScheme:
     def images_to_colors(self, images_path=None, number_of_colors=5, compress_to=300):
         """
         Read a list of images from 'images_path' and convert them into the most popular colors.
+        Save the list of colors into 'self.colors_path' file.
 
         :param images_path: (str) path to saved numpy array of images produced by 'video_to_images()' function.
             If None, use the value stored in the current object.
-        :param number_of_colors: (int)
+        :param number_of_colors: (int) number of colors to cluster the image.
+            Large values slow down the analysis
         :param compress_to: (int) number of pixels to resize the image during preprocessing, usually 300-500.
             Large values slow down the analysis, small values skew its results
         :return: (str) path to saved array of colors
@@ -125,7 +125,7 @@ class VideoColorScheme:
         return self.__analyze_colors(modified_image, number_of_colors)
 
     @staticmethod
-    def __preprocess(image, compress_to=500):
+    def __preprocess(image, compress_to):
         """ Preprocess the image for cluster analysis: compress it and reshape to 2D array """
 
         image = cv2.resize(image, (compress_to, compress_to))  # resize the image to speed up the analysis
@@ -133,7 +133,7 @@ class VideoColorScheme:
         return image
 
     @staticmethod
-    def __analyze_colors(image, number_of_colors=5):
+    def __analyze_colors(image, number_of_colors):
         """
         Return the list of colors for the image.
         List contains of tuples (color, ratio) and contains 'number_of_colors' elements
@@ -153,18 +153,22 @@ class VideoColorScheme:
         color_counts = Counter(color_labels)  # the number of times each colors occurs on the image
         total = sum(color_counts.values())
         ordered_colors = [center_colors[idx] for idx in color_counts.keys()]  # colors ordered by popularity
+        if len(ordered_colors) != number_of_colors:
+            # if the clustering algorithm predicts fewer colors than we expect
+            # (for example, if the image is uni-color)
+            return [(rgb_to_hex(ordered_colors[0]), 1)]
         hex_colors = [(rgb_to_hex(ordered_colors[idx]), value / total) for idx, value in color_counts.items()]
         return hex_colors
 
     def choose_colors(self, colors_path=None, mode='sample'):
         """
-        Choose a list of color based on color analysis
+        Choose a list of colors based on color analysis
 
         :param colors_path: (str) path to saved numpy array of colors produced by 'images_to_colors()' function.
             If None, use the value stored in the current object.
         :param mode: (str) mode to choose a color.
             If 'popular' - return the most popular color, else sample a color according to distribution
-        :return: result_colors: (list of str) colors that will be used in the result image
+        :return: result_colors: (list of str) colors that will be used to compose the result image
         """
 
         if colors_path is None:
@@ -193,7 +197,7 @@ class VideoColorScheme:
     @staticmethod
     def __choose_color(color_tuple, mode):
         """
-        Choose a color from color tuple
+        Choose a color from the color tuple
 
         :param color_tuple: (list of tuples) containing colors and their ratios on image, sorted by popularity
         :param mode: (str) mode to choose a color.
@@ -205,15 +209,18 @@ class VideoColorScheme:
             return color_tuple[0][0]  # the first color
         else:
             colors, prob = zip(*color_tuple)
+            if isinstance(colors, str):
+                return colors  # single color - uni-color image
             return np.random.choice(colors, p=prob)
 
-    def compose_result_image(self, result_colors, width, height):
+    def compose_result_image(self, result_colors, width, height, result_path):
         """
-        Create the result image from an array of colors and save it into 'self.result_path'
+        Create the result image from an array of colors and save it into 'result_path'
 
         :param result_colors: (list of str) list of the HEX colors to create an image
         :param height: (int) height of the result image, inch
         :param width: (int) weight of the result image, inch
+        :param result_path: (str) path to save the result image
         """
 
         total_colors = len(result_colors)
@@ -235,7 +242,7 @@ class VideoColorScheme:
 
         plt.axis('off')
         plt.margins(x=0, y=0)  # remove blank space around the plot
-        plt.savefig(self.result_path, dpi=300, bbox_inches='tight')
+        plt.savefig(result_path, dpi=300, bbox_inches='tight')
         plt.show()
         print("The result images was successfully composed")
 
