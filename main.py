0479484fad3d7b80d308d75af0b69c9e006217dd
Winston-90 <nikolaevdmitryprog@gmail.com>
2022-05-14 13:09:14 -0400

    Reformat main

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ def parse_args(_):
 
     # required arguments
     parser.add_argument('--video_path', type=str, required=True,
-                        default='videos/short.mp4', help='''Path to the video to process''')
+                        default="videos/batman.mp4",
+                        help='''Path to the video to process''')
     parser.add_argument('--result_path', type=str, required=True,
-                        default='videos/short.png', help='''Path to save the result image (including .png)''')
+                        default="videos/batman.png",
+                        help='''Path to save the result image (including .png)''')
     parser.add_argument('--width', type=float, required=True,
-                        default=20, help='''Width of the result image, inches''')
+                        default=25, help='''Width of the result image, inches''')
     parser.add_argument('--height', type=float, required=True,
                         default=5, help='''Height of the result image, inches''')
 
@@ -27,23 +29,30 @@ def parse_args(_):
                         default=1., help='''Step in seconds''')
     parser.add_argument('--number_of_colors', type=float, required=False,
                         default=5, help='''Number of colors to extract from each image''')
+    parser.add_argument('--compress_to', type=int, required=False,
+                        default=200, help='''Number of pixels to resize the image during preprocessing''')
+    parser.add_argument('--verbose', type=bool, required=False,
+                        default=False, help='''If True, show the result image''')
     parser.add_argument('--delete_after', type=bool, required=False,
-                        default=True, help='''If True, delete temporary files after processing''')
+                        default=False, help='''If True, delete temporary files after processing''')
 
     return parser.parse_args()
 
 
 def main(args):
-    v = VideoColorScheme(video_path=args.video_path, temp_folder='tmp', delete_after=False)
+    v = VideoColorScheme(video_path=args.video_path, temp_folder='tmp', delete_after=args.delete_after)
 
     # convert initial video to array of images
     images_path = v.video_to_images(args.start, args.step)
+
     # extract the most popular image colors
-    colors_path = v.images_to_colors(images_path, args.number_of_colors)
+    colors_path = v.images_to_colors(images_path, args.number_of_colors, args.compress_to)
+
     # choose the colors for the result image
     result_colors = v.choose_colors(colors_path, mode=args.mode)
     # compose a result image from an array of colors
-    v.compose_result_image(result_colors, width=args.width, height=args.height, result_path=args.result_path)
+    v.compose_result_image(result_colors, width=args.width, height=args.height,
+                           result_path=args.result_path, verbose=args.verbose)
 
 
 if __name__ == '__main__':
